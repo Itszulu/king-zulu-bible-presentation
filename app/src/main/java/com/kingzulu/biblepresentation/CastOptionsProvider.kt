bb2626f3e105bb3978ca9dc416e1e26a7951314d
Itszulu <74699213+Itszulu@users.noreply.github.com>
2026-09-17 22:59:58 +0100

    Use registered King Zulu Cast receiver application

--- app/src/main/java/com/kingzulu/biblepresentation/CastOptionsProvider.kt
+++ app/src/main/java/com/kingzulu/biblepresentation/CastOptionsProvider.kt
@@ -1,15 +1,21 @@
 package com.kingzulu.biblepresentation
 
 import android.content.Context
-import com.google.android.gms.cast.CastMediaControlIntent
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
 
+/** Google Cast sender configuration for the registered King Zulu receiver. */
 class CastOptionsProvider : OptionsProvider {
+    companion object {
+        const val RECEIVER_APPLICATION_ID = "86654938"
+        const val PRESENTATION_NAMESPACE = "urn:x-cast:com.kingzulu.presentation"
+    }
+
     override fun getCastOptions(context: Context): CastOptions =
         CastOptions.Builder()
-            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .setReceiverApplicationId(RECEIVER_APPLICATION_ID)
+            .setStopReceiverApplicationWhenEndingSession(false)
             .build()
 
     override fun getAdditionalSessionProviders(context: Context): List<SessionProvider>? = null
